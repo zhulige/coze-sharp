3be2f3871079786cc2b95a9e81777343a7ac8df2
朱利戈 <zhulige@hotmail.com>
2025-03-08 13:04:05 +0800

    添加使用说明

--- CozeSharp_Test/Program.cs
+++ CozeSharp_Test/Program.cs
@@ -8,7 +8,20 @@ class Program
     {
         Console.ForegroundColor = ConsoleColor.Green;
         Console.Title = "扣子AI 调试助手";
+        string logoAndCopyright = @"
+========================================================================
+欢迎使用“扣子AI 服务器调试控制台” ！版本 v1.0.1
 
+当前功能：
+1. 语音消息 输入回车：开始录音；再次输入回车：接收录音
+2. 文字消息 可以随意输入文字对话
+3. 全量往返协议输出，方便调试
+        
+要是你在使用中有啥想法或者遇到问题，别犹豫，找我们哟：
+微信：Zhu-Lige       电子邮箱：ZhuLige@qq.com
+有任何动态请大家关注 https://github.com/zhulige/coze-sharp
+========================================================================";
+        Console.WriteLine(logoAndCopyright);
         _cozeAgent = new CozeAgent();
 
         while (true)
