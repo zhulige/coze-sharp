5fb053dfd07926e87ce781cb997d6bfc66070f58
朱利戈 <zhulige@hotmail.com>
2025-06-08 16:22:14 +0800

    维护Nuget信息

--- CozeSharp/Serivces/AudioWaveService.cs
+++ CozeSharp/Serivces/AudioWaveService.cs
@@ -3,8 +3,10 @@ using NAudio.Wave;
 using OpusSharp.Core;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CozeSharp.Serivces
